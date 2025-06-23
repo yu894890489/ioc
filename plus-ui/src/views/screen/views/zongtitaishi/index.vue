a08837efc2115315f635271c307c66ff79a69168
wujian <2457205703@qq.com>
2025-06-23 11:05:32 +0800

    将左右两边的渐变背景色切换为tailwindcss

--- plus-ui/src/views/screen/views/zongtitaishi/index.vue
+++ plus-ui/src/views/screen/views/zongtitaishi/index.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="w-full h-full flex flex-nowrap justify-between items-center ">
-        <div class="h-full flex flex-col gap-14px bg-left">
+        <div class="h-full flex flex-col gap-14px pt-100px 
+        bg-[linear-gradient(90deg,#001721_0%,rgba(0,32,43,0.8)_33%,rgba(0,51,64,0.41)_92%,rgba(0,71,86,0)_100%)]">
             <zongtitaishiLeft></zongtitaishiLeft>
         </div>
 
-        <div class="h-full flex flex-col gap-14px bg-right">
+        <div class="h-full flex flex-col gap-14px pt-100px 
+        bg-[linear-gradient(-90deg,#001721_0%,rgba(0,32,43,0.8)_33%,rgba(0,51,64,0.41)_92%,rgba(0,71,86,0)_100%)]">
             <zongtitaishiRight></zongtitaishiRight>
         </div>
     </div>
@@ -14,19 +16,4 @@ import zongtitaishiLeft from './zongtitaishi-left.vue'
 import zongtitaishiRight from './zongtitaishi-right.vue'
 
 </script>
-<style lang="scss" scoped>
-.bg-left {
-    background: linear-gradient(90deg,
-            #001721 0%, rgba(0, 32, 43, 0.8) 33%,
-            rgba(0, 51, 64, 0.41) 92%,
-            rgba(0, 71, 86, 0) 100%);
-}
-
-.bg-right {
-    background: linear-gradient(-90deg,
-            #001721 0%,
-            rgba(0, 32, 43, 0.8) 33%,
-            rgba(0, 51, 64, 0.41) 92%,
-            rgba(0, 71, 86, 0) 100%);
-}
-</style>
+<style lang="scss" scoped></style>
